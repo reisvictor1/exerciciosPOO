7a543afe937d81da838c16e6c13ec94c00c33c2b
reisvictor1 <36137949+reisvictor1@users.noreply.github.com>
2019-05-07 11:35:48 -0300

    Add files via upload
    Exercício de grafos em POO/JAVA

---
+++ Grafo.java
@@ -0,0 +1,56 @@
+package grafos_ex;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Grafo {
+
+    File entry;
+    Map<String,HashMap<String, Integer>> vert; 
+    Scanner reader;
+    public Grafo(String filename) throws FileNotFoundException {
+        
+        entry  = new File(filename);
+        reader = new Scanner(entry);
+        vert = new HashMap<String, HashMap<String,Integer>>();
+     
+        
+    }
+    
+    
+    
+    void CriarGrafo(){
+        
+        String nome_cidade;
+        HashMap<String, Integer> adj;
+        
+        while(reader.hasNext()){
+            String adj_cidade;
+            int dist;
+            nome_cidade = reader.next();
+            
+            adj = new HashMap<String,Integer>();
+            
+            while(!reader.hasNextInt()){
+                adj_cidade = reader.next();
+                dist = reader.nextInt();
+                
+                adj.put(adj_cidade,dist);
+                
+            }
+            
+            vert.put(nome_cidade, adj);
+           
+        }
+        
+    }
+    
+    
+    
+   
+}
---
+++ Grafos_ex.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafos_ex;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIOS+10734686
+ */
+public class Grafos_ex extends Grafo{
+
+    
+    public static void main(String[] args) {
+     
+      
+        Scanner reader;
+        reader = new Scanner(System.in);
+        String filename = reader.toString();
+        Grafo cidades = null;
+        
+        try{
+            cidades = new Grafo(filename);
+        }
+        catch(Exception e){
+            System.out.println("Deu erro ao abrir o arquivo");
+        }
+        
+        cidades.criaGrafo();
+        
+    }
+    
+}
